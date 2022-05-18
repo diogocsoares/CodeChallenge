bbd7740a8215149737bfbf15f9d1f634b34a44cc
Soares <DSOARES2@bethel.jw.org>
2022-05-18 14:18:37 -0400

    Minimum Deph

--- javascript/TreeGraph/minimumDepthBinaryTree.js
+++ javascript/TreeGraph/minimumDepthBinaryTree.js
@@ -35,23 +35,27 @@ function createTree(array) {
    ++point;
    while (point < array.length) {
       if (array[point] != null) {
-         if (point % 2 === 0)
-            tree.push(new TreeNode(array[point], array[point + left + 1], array[point + right + 1]));
-         else
+         if (point % 2 === 0) {
+            left = array[point - 1] === null ? 1 : 3;
+            right = array[point - 1] === null ? 2 : 4;
             tree.push(new TreeNode(array[point], array[point + left], array[point + right]));
+         }
+         else {
+            left =  1;
+            right = 2;      
+            tree.push(new TreeNode(array[point], array[point + left + 1], array[point + right + 1]));
+         }
          if (tree.length * 2 + 1 === array.length)
             point = array.length;
       }
-      left = array[point + 1] === null ? 2 : 4;
-      right = array[point + 1] === null ? 1 : 3;
       ++point;
    }
    console.log(tree);
    return tree;
 }
 
-//createTree([3, 9, 20, null, null, 15, 7])
-//createTree([2, null, 3, null, 4, null, 5, null, 6])
+createTree([3, 9, 20, null, null, 15, 7])
+createTree([2, null, 3, null, 4, null, 5, null, 6])
 createTree([2, 3, null, 4, null, 5, null, 6, null])
 
 module.exports = { TreeNode, minDepth, createTree }
