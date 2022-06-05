b899aa341534b1bcae60b7062805bfbc4106eb25
diogocsoares <90847191+diogocsoares@users.noreply.github.com>
2022-06-05 16:40:47 -0400

    Binary search tree

---
+++ study/dataStructures/trees/binarySearchTrees.js
@@ -0,0 +1,176 @@
+//They are grate to comparing things. This data structure preserves relationships.
+//Rules: 1 - All child in the tree to the right of the root node must be grater than the current node. 2 - A node can only have up to two children because it's a binary tree.
+
+//https://visualgo.net/en/bst
+
+//Problem: Unbalance binary trees can be behavior like a linked list with time complexity O(n) in lookup, insert and delete operations.
+
+// If you would like to learn more about these types of trees where the complexity comes from the auto-balancing of trees upon insertion, these are the resources that I recommend but keep in mind that they are not SUPER important for an interview. Just to scratch your curiosity :)
+
+// AVL Trees:
+
+// Animation https://www.cs.usfca.edu/~galles/visualization/AVLtree.html
+// How it Works https://medium.com/basecs/the-little-avl-tree-that-could-86a3cae410c7
+
+// Red Black Trees:
+
+// Animation https://www.cs.usfca.edu/~galles/visualization/RedBlack.html
+// How it Works https://www.cs.usfca.edu/~galles/visualization/RedBlack.html
+
+// You can compare the technical details between the two here https://stackoverflow.com/questions/13852870/red-black-tree-over-avl-tree
+
+
+class TreeNode {
+   constructor(value) {
+      this.left = null;
+      this.right = null;
+      this.value = value;
+   }
+}
+
+class BinarySearchTree {
+   constructor() {
+      this.root = null;
+   }
+
+   insert(value) {
+      const node = new TreeNode(value);
+      if (this.root === null) {
+         this.root = node;
+         return true;
+      }
+      let currentNode = this.root;
+      while (currentNode) {
+         if (value > currentNode.value) {
+            if (!currentNode.right) {
+               currentNode.right = node;
+               return true;
+            } else {
+               currentNode = currentNode.right;
+            }
+         } else {
+            if (!currentNode.left) {
+               currentNode.left = node;
+               return true;
+            } else {
+               currentNode = currentNode.left;
+            }
+         }
+      }
+      return false;
+   }
+
+   lookup(value) {
+      if (!this.root)
+         return false;
+      let currentNode = this.root;
+      while (currentNode) {
+         if (currentNode.value === value)
+            return currentNode;
+         if (value > currentNode.value)
+            currentNode = currentNode.right;
+         else
+            currentNode = currentNode.left;
+      }
+      return false;
+   }
+
+   remove(value) {
+      if (!this.root) {
+         return false;
+      }
+      let currentNode = this.root;
+      let parentNode = null;
+
+      while (currentNode) {
+         if (value < currentNode.value) {
+            parentNode = currentNode;
+            currentNode = currentNode.left;
+         } else if (value > currentNode.value) {
+            parentNode = currentNode;
+            currentNode = currentNode.right;
+         } else if (currentNode.right === value) {
+
+            //Option 1: No Right child:
+            if (currentNode.right === null) {
+               if (parentNode === null) {
+                  this.root = currentNode.left;
+               } else {
+                  // if parent < current value, make left child a child of parent
+                  if (currentNode.value < parentNode.value) {
+                     parentNode.left = currentNode.left;
+
+                     //if parent < current value, make left child a right child of parent
+                  } else if (currentNode.value > parentNode.value) {
+                     parentNode.right = currentNode.lef;
+                  }
+               }
+               //Option 2: Right child that has a left child
+            } else if (currentNode.right.left === null) {
+               if (parentNode == null) {
+                  this.root = currentNode.left;
+               } else {
+                  currentNode.right.left = currentNode.left;
+
+                  //If parent > current, make right child of the left the parent.
+                  if (currentNode.value < parentNode.value) {
+                     parentNode.left = currentNode.right;
+
+                     //if parent < current, make right child a right child of the parent;
+                  } else if (currentNode.value > parentNode.value) {
+                     parentNode.right = currentNode.right;
+                  }
+               }
+               //Option 3: Right child that has a left child
+            } else {
+               //Find the right child's left most child.
+               let leftMost = currentNode.Right.left;
+               let leftMostParent = currentNode.right;
+               while (leftMost.left !== null) {
+                  leftMostParent = leftMost;
+                  leftMost = leftMost.left;
+               }
+
+               //Parent's left subtree is now leftMost's right subtree.
+               leftMostParent.left = leftMost.right;
+               leftMost.left = currentNode.left;
+               leftMost.right = currentNode.right;
+
+               if (parentNode === null) {
+                  this.root = leftMost;
+               } else {
+                  if (currentNode.value < parentNode.value) {
+                     parentNode.left = leftMost;
+                  } else if (currentNode.value > parentNode.value) {
+                     parentNode.right = leftMost;
+                  }
+               }
+            }
+            return true;
+         }
+      }
+   }
+}
+
+function traverse(node) {
+   const tree = { value: node.value };
+   tree.left = node.left === null ? null : traverse(node.left);
+   tree.right = node.right === null ? null : traverse(node.right);
+   return tree;
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+//tree.remove(170); //don't work can fix
+//console.log(tree.lookup(4));
+
+console.log(JSON.stringify(traverse(tree.root)));
+
+
+
---
+++ study/dataStructures/trees/trees.js
@@ -0,0 +1,27 @@
+// Trees are a data structure that have what we call a hierarchical structure, as opposed to something like linked lists or arrays, which are linear. Trees can have zero or more child nodes. A tree usually starts with a single root node or a parent node, and every child of the tree descends form this root node. So it's kind of like a inverse tree. Every child of a node descends from only one parent. So we have a parent child relationship that you see is unidirectional, that is, it's gone one way, and you also have leaf nodes, which are the very end of a tree data structure. Finally within a tree, we can have things like sub trees.
+
+//Abstract syntax tree is how programs or code is usually being run. Usually we write code and then its gets broken down by the machine into this abstract syntax tree so that it understands what we wrote down. This uses a tree data structure.
+
+// Rules: 1 - Each node can only have either zero, one or two nodes. 2 - Each child can only have one parent. 3 - Each node represents a certain state.
+
+//Example:
+function BinaryTreeNodeExample(value) {
+   this.value = value;
+   this.left = null;
+   this.right = null;
+}
+
+//Perfect Binary Tree: has everything filled in that means all the leaf nodes are full and there's node that only has one child. The bottom layer of the tree is completely filled. It is important because this type of binary tree is really efficient. This type has two special properties: 1 - The number of total nodes on each level doubles as we move down the tree. 2 - The number of nodes on the last level is equal to the sum of the number of nodes on all the other level plus one.
+//Time complexity: Lookup O(log N), insert O(log N), delete O(log N).
+
+//Examples: 
+//Level 0: 2ˆ0 = 1;
+//Level 1: 2ˆ1 = 2;
+//Level 2: 2ˆ2 = 4;
+//Level 3: 2ˆ3 = 8;
+
+// # of nodes = 2ˆh - 1 (h is height or levels, steps).
+// simplifying log nodes = height > log 100 = 2 because 10ˆ2 = 100
+// Log is simply means that based on the height, the maximum number od decisions it's going to be log N. One decision for each level or step. In a tree with 3 levels we have 3 decision to make in a worst case.
+
+//Full Binary Tree: Which simply says that a node has either a zero or two children, but never one child.
