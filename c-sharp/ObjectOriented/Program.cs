06f871d83149f36cb0163f20b9d4c926868303f9
Soares <DSOARES2@bethel.jw.org>
2022-06-13 19:10:04 -0400

    Exceptions and Object Oriented

---
+++ c-sharp/ObjectOriented/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectOriented
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
