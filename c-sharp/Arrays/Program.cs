fadd1f9319bdf90438233b1e1c5b5fb223e2f185
Soares <DSOARES2@bethel.jw.org>
2022-06-13 16:55:28 -0400

    Array

---
+++ c-sharp/Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myArray = new int[10]; //must have an length.
+            int[] myArray2 = new int[3] {1, 3, 8}; // Initializing each position.
+            myArray[0] = 10; 
+            var arrayPosition = myArray[0];
+            Console.WriteLine(arrayPosition);
+            Console.WriteLine(myArray2[2]);
+
+            var myArrayStruct = new ObjStructArray[2] {new ObjStructArray(), new ObjStructArray() };
+            myArrayStruct[0].Id = 15;
+            myArrayStruct[1].Id = 22;
+            Console.WriteLine(myArrayStruct[0].Id);
+            
+        }
+
+        struct ObjStructArray
+        {
+            public int Id;
+        }
+
+    }
+}
