8be2b8b554fa54e1667c90173b2e57af7eeea522
diogocsoares <90847191+diogocsoares@users.noreply.github.com>
2022-07-05 01:47:25 -0400

    patial

--- javascript/Patterns/Design/designWordSearchDataStructure.js
+++ javascript/Patterns/Design/designWordSearchDataStructure.js
@@ -8,35 +8,49 @@ class TrieNode {
 class WordDictionary {
    constructor() {
       this.root = new TrieNode();
+      this.root.children['.'] = new TrieNode()
    }
 
    addWord(word) {
       let currentNode = this.root;
+      let dotNode = this.root.children['.'];
+      let dotWord = new Array(word.length).fill('.');
       for (let i = 0; i < word.length; i++) {
          if (!(word[i] in currentNode.children)) {
             currentNode.children[word[i]] = new TrieNode();
+            if (word[i])
+               dotNode.children[word[i]] = new TrieNode();
+         }
+
+         dotWord[i] = word[i];
+         for (let j = 0; j < dotWord.length; j++) {
+            if (!(dotWord[j] in dotNode.children)) {
+               dotNode.children[dotWord[j]] = new TrieNode();
+            }
+            dotNode = dotNode.children[dotWord[j]];
          }
          currentNode = currentNode.children[word[i]];
       }
       currentNode.endOfWord = true;
+      dotNode.endOfWord = true
    }
 
    search(word) {
       let currentNode = this.root;
       for (let i = 0; i < word.length; i++) {
-         if (word[i] === '.') {
-            for (let char of Object.keys(currentNode.children)) {
-               let anyWord = [...word];
-               anyWord[i] = char;
-               if (this.search(anyWord.join('')))
-                  return true;
-            }
+         //         if (word[i] === '.') {
+         //            for (let char of Object.keys(currentNode.children)) {
+         //               let anyWord = [...word];
+         //              anyWord[i] = char;
+         //               if (this.search(anyWord.join('')))
+         //                  return true;
+         //            }
+         //            return false;
+         //         } else {
+         if (!(word[i] in currentNode.children))
             return false;
-         } else {
-            if (!(word[i] in currentNode.children))
-               return false;
-            currentNode = currentNode.children[word[i]];
-         }
+         currentNode = currentNode.children[word[i]];
+         //       }
       }
       return currentNode.endOfWord === true;
    }
@@ -46,9 +60,8 @@ let trie = new WordDictionary()
 trie.addWord('bad');
 trie.addWord('dad');
 trie.addWord('mad');
+//trie.addWord('.ad');
+//trie.addWord('b..');
 console.log(trie.search('dad'))
 console.log(trie.search('.ad'));
 console.log(trie.search('b..'));
-
-// ["WordDictionary", "addWord", "addWord", "search", "search", "search", "search", "search", "search"]
-// [[],                ["a"],     ["a"],     ["."],    ["a"],    ["aa"],   ["a"],    [".a"],   ["a."]]
