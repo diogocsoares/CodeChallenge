2a7535d1c592d308ac56299ff9635c712c4fa0aa
diogocsoares <90847191+diogocsoares@users.noreply.github.com>
2022-07-04 22:30:12 -0400

    Conclude Trie implementation

--- javascript/DataStructures/Tries/trie.js
+++ javascript/DataStructures/Tries/trie.js
@@ -1,7 +1,6 @@
 class TrieNode {
-    constructor (char) {
-        if (char)
-            this.children = {};
+    constructor() {
+        this.children = {};
         this.endOfWord = false;
     }
 }
@@ -11,35 +10,44 @@ class Trie {
         this.root = new TrieNode();
     }
 
-    insert(word){
+    insert(word) {
         let currentNode = this.root;
-        for (let i=0; i < word.length; i++) {
-            let value = currentNode.children.word[i] === undefined
-            if (currentNode.children.word[i] === undefined)
+        for (let i = 0; i < word.length; i++) {
+            if (!(word[i] in currentNode.children)) {
                 currentNode.children[word[i]] = new TrieNode();
+            }
+            currentNode = currentNode.children[word[i]];
         }
         currentNode.endOfWord = true;
     }
 
-    search(word){
+    search(word) {
         let currentNode = this.root;
-        for (let i = 0; i < word.length; i++){
-            if (currentNode.children[word[i]] === undefined)
+        for (let i = 0; i < word.length; i++) {
+            if (!(word[i] in currentNode.children))
                 return false;
+            currentNode = currentNode.children[word[i]];
         }
         return currentNode.endOfWord === true;
     }
 
     startWith(prefix) {
         let currentNode = this.root;
-        if (currentNode.children[prefix[i]] === undefined)
-        return false;
-
+        for (let i = 0; i < prefix.length; i++) {
+            if (!(prefix[i] in currentNode.children))
+                return false;
+            currentNode = currentNode.children[prefix[i]];
+        }
         return true;
     }
 }
 
 let trie = new Trie();
 trie.insert('diogo');
-console.log(trie.search('diog'));
-console.log(trie.startWith('dddio'));
+trie.insert('diego');
+trie.insert('daniel');
+trie.insert('carla');
+trie.insert('carlos');
+console.log(trie.search('diogo'));
+console.log(trie.search('carl'));
+console.log(trie.startWith('ccarla'));
