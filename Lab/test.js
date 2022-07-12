184f368366815d5c4a0bd63ba3df8f74ec99526a
diogocsoares <90847191+diogocsoares@users.noreply.github.com>
2022-07-12 09:05:45 -0400

    Organizing folders and files

--- Lab/test.js
+++ Lab/test.js
@@ -1,19 +1,3 @@
-const details = {
-   message: 'Hello World!'
-};
-
-function getMessage() {
-   return this.message;
-}
-
-console.log(getMessage.apply(details));
-
-const person = {
-   name: "Marko Polo"
-};
-
-function greeting(greetingMessage) {
-   return `${greetingMessage} ${this.name}`;
-}
-
-console.log(greeting.apply(person, ['Hello'])); // returns "Hello Marko Polo!"
+var a = [1];
+delete a[1]
+console.log(a.length)
