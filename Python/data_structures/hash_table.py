13e63066557531e9f4a2dc3bd77afd04a7651757
Diogo Soares <diogo@dicantelitente.com.br>
2023-01-30 16:39:44 -0300

    Bug fix on hashtable implementation in js

--- Python/data_structures/hash_table.py
+++ Python/data_structures/hash_table.py
@@ -1,3 +1,5 @@
+# JavaScript Way translated to Python
+
 class HashTable:
     def __init__(self, size):
         self.data = [None for elem in range(size)]
@@ -22,7 +24,7 @@ class HashTable:
         for i in range(len(point_data)):
             if point_data[i][0] == key:
                 return point_data[i][1]
-        return point_data
+        return point_data[1]
 
     def keys(self):
         keys_array = []
@@ -33,7 +35,56 @@ class HashTable:
         return keys_array
 
 
+print("JavaScript Way translated to Python")
 myHashTable = HashTable(10)
+myHashTable.set('grapes', 1000)
+myHashTable.set('lemon', 100)
+myHashTable.set('apple', 330)
+myHashTable.set('orange', 54)
+print(myHashTable.get('apple'))
+print(myHashTable.keys())
+
+# Pythonic code
+
+
+class PyHashTable:
+    def __init__(self, size):
+        self.data = [None for elem in range(size)]
+
+    def hash_function(self, key):
+        _hash = 0
+        for i in range(len(key)):
+            _hash = (_hash + ord(key[i]) * i) % len(self.data)
+        return _hash
+
+    def set(self, key, value):
+        point = self.hash_function(key)
+        if not self.data[point]:
+            self.data[point] = []
+            self.data[point].append([key, value])
+        else:
+            self.data[point].append([key, value])
+        return True
+
+    def get(self, key):
+        point_data = self.data[self.hash_function(key)]
+        for point_key in point_data:
+            if point_key == key:
+                return point_key[1]
+        return point_data[0][1]
+
+    def keys(self):
+        keys_array = []
+        for point_key in self.data:
+            if point_key:
+                for value in point_key:
+                    keys_array.append(value[0])
+        return keys_array
+
+
+print("")
+print("Pytonic way")
+myHashTable = PyHashTable(10)
 myHashTable.set('grapes', 1000)
 myHashTable.set('lemon', 100)
 myHashTable.set('apple', 330)
