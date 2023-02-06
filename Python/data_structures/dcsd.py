2a922ac367ab15b66dcd0d1818a91a5f46203e22
Diogo Soares <diogo@dicantelitente.com.br>
2023-02-06 11:43:24 -0300

    Lab Ordering lists

--- Python/data_structures/dcsd.py
+++
@@ -1,6 +0,0 @@
-s = "red2 blue5 black4 green1 gold3"
-colors = s.split()
-for element in colors.sort(key=lambda colors: colors[len(colors)-1]):
-    element = element[0:len(element)-1]
-
-print(colors)
---
+++ Python/lab/order-list01.py
@@ -0,0 +1,19 @@
+
+print("===== Way 1 =====")
+imput = "red2 blue5 black4 green1 gold3"
+colors = imput.split()
+colors.sort(key=lambda colors: colors[len(colors)-1])
+print(colors)
+ordered_colors = []
+for color in colors:
+    ordered_colors.append(color[:len(color)-1])
+print(ordered_colors)
+
+print("===== Way 2 =====")
+imput = "red2 blue5 black4 green1 gold3"
+colors = imput.split()
+colors.sort(key=lambda colors: colors[len(colors)-1])
+print(colors)
+ordered_colors = list(map(lambda color: color[:len(
+    color)-1], colors))
+print(ordered_colors)
