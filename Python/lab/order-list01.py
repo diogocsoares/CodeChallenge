8eab13fd906717ade2f5733121d12aa78c802163
Diogo Soares <diogo@dicantelitente.com.br>
2023-02-06 11:46:05 -0300

    Lab Ordering lists

--- Python/lab/order-list01.py
+++ Python/lab/order-list01.py
@@ -14,6 +14,5 @@ imput = "red2 blue5 black4 green1 gold3"
 colors = imput.split()
 colors.sort(key=lambda colors: colors[len(colors)-1])
 print(colors)
-ordered_colors = list(map(lambda color: color[:len(
-    color)-1], colors))
+ordered_colors = list(map(lambda color: color[:len(color)-1], colors))
 print(ordered_colors)
